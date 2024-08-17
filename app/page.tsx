af05fd736cdc9a86daa04a096ecf60e7c1294991
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-08-17 02:23:53 -0400

    Add code to chat with the LLM using a hard coded question

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
+import { Chat } from 'ragged';
+
 export default function Home() {
+  let chatReply: string | null | undefined;
+  
+  const chatInstance = Chat.with({
+    provider: 'cohere',
+    config: { apiKey: process.env.COHERE_API_KEY, model: 'command-light' }
+  });
+
+  async function getResponse() {
+    const { history } = await chatInstance.chat('What is a rickroll?');
+    chatReply = history.at(-1)?.text;
+    console.log(chatReply);
+  }
+
+  getResponse().catch(e => console.error(e));
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
