675c8784b11b3ac399242adcfffbc2f1735ee0ea
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-08-20 14:44:13 -0400

    Update the component to display the chat reply in the Conversation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
 import { Chat } from 'ragged';
 
-export default function Home() {
-  let chatReply: string | null | undefined;
-  
+import Conversation from './ui/conversation';
+
+export default async function Home() {
   const chatInstance = Chat.with({
     provider: 'cohere',
     config: { apiKey: process.env.COHERE_API_KEY, model: 'command-light' }
   });
 
-  async function getResponse() {
-    const { history } = await chatInstance.chat('What is a rickroll?');
-    chatReply = history.at(-1)?.text;
-    console.log(chatReply);
-  }
-
-  getResponse().catch(e => console.error(e));
+  const { history } = await chatInstance.chat('What is a rickroll?');
+  const chatReply = history.at(-1)?.text;
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Conversation chatData={chatReply} />
     </main>
   );
 }
