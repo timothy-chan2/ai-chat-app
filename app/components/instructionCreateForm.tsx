7267f9661889e754ab96adbb674b912b6934156b
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-08-23 02:51:18 -0400

    Update so there is feedback to show when the AI is searching for the answer after pressing the send button

--- app/components/instructionCreateForm.tsx
+++ app/components/instructionCreateForm.tsx
@@ -1,11 +1,30 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
 import { getAnswer } from '../actions';
+import React from 'react';
+
+type SubmitButtonProps = {
+  label: string,
+  loading: React.ReactNode
+};
+
+const SubmitButton = ({ label, loading }: SubmitButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button disabled={pending} type='submit'>
+      {pending ? loading : label}
+    </button>
+  );
+};
 
 const InstructionCreateForm = () => {
   return (
     <form action={getAnswer}>
       <label htmlFor='instruction'>Instruction</label>
       <textarea id='instruction' name='instruction' />
-      <button type='submit'>Send</button>
+      <SubmitButton label='Send' loading='Searching...' />
     </form>
   );
 };
