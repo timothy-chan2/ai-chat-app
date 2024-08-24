b2b0a406e546e82e26b64c2a6ed293007e6bc0f8
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-08-24 17:24:16 -0400

    Move the content of ClientComponent into the Home function

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import { ClientComponent } from './components/clientComponent';
 import { MessageList } from './components/messageList';
 import { InstructionCreateForm } from './components/instructionCreateForm';
 
 export default function Home() {
-  // https://www.robinwieruch.de/next-forms/
-  
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <ClientComponent />
+      <h1>AI Chat</h1>
       <MessageList />
       <InstructionCreateForm />
     </main>
