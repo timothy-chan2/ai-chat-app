43879fe7eca84360c75e854f695b2adf712668e3
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-09-13 00:39:24 -0400

    Update the vertical spacing between the elements so they are closer together

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { InstructionCreateForm } from './components/instructionCreateForm';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>AI Chat</h1>
+    <main className='flex min-h-screen flex-col justify-center items-center p-24'>
+      <h1 className='mb-10 text-3xl'>AI Chat</h1>
       <MessageList />
       <InstructionCreateForm />
     </main>
