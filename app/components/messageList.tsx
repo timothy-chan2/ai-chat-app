ac59c381ec682b8e8465205ccc7c7455abe6b066
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-09-13 00:06:00 -0400

    Update to hide the empty unordered list element when there are no messages to display

--- app/components/messageList.tsx
+++ app/components/messageList.tsx
@@ -2,8 +2,10 @@ import { getMessages } from '../actions';
 
 const MessageList = async () => {
   const messages = await getMessages();
+  const numberOfMessages = messages.length;
 
   return (
+    numberOfMessages > 0 &&
     <ul>
       {messages.map(message => (
         <li key={message.id}>
