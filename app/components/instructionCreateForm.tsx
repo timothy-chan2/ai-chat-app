d05a8f9287cdbc5600e3f81f6b44f7820aa5346e
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-09-13 00:36:26 -0400

    Update the textarea background to a dark color so the text in white is visible

--- app/components/instructionCreateForm.tsx
+++ app/components/instructionCreateForm.tsx
@@ -7,7 +7,7 @@ const InstructionCreateForm = () => {
   return (
     <form action={getAnswer} className='flex flex-col items-center'>
       <label htmlFor='instruction'>Instruction</label>
-      <textarea id='instruction' name='instruction' />
+      <textarea id='instruction' name='instruction' className='bg-white/20 mt-1 mb-5' />
       <SubmitButton label='Send' loading='Searching...' />
     </form>
   );
