2bbbd329939ab96407b58e6cc31ffdfaa7e2e8f4
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-09-25 01:08:59 -0400

    Reduce the padding of the main element so the contents fill most of the width of mobile screens

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { InstructionCreateForm } from './components/instructionCreateForm';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col justify-center items-center p-24'>
+    <main className='flex min-h-screen flex-col justify-center items-center p-5'>
       <h1 className='mb-10 text-3xl'>AI Chat</h1>
       <MessageList />
       <InstructionCreateForm />
