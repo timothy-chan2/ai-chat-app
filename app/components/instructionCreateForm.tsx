2edf54e9622c47c84836526ab242fc980dfd599c
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-10-02 23:53:19 -0400

    Reduce the width of the form element on large screens for a nicer appearance

--- app/components/instructionCreateForm.tsx
+++ app/components/instructionCreateForm.tsx
@@ -5,7 +5,7 @@ import { SubmitButton } from './submitButton';
 
 const InstructionCreateForm = () => {
   return (
-    <form action={getAnswer} className='flex flex-col items-center w-full'>
+    <form action={getAnswer} className='flex flex-col items-center w-full lg:w-1/3'>
       <label htmlFor='instruction'>Instruction</label>
       <textarea id='instruction' name='instruction' className='bg-white/20 mt-1 mb-5 resize-none w-full h-20' />
       <SubmitButton label='Send' loading='Searching...' />
