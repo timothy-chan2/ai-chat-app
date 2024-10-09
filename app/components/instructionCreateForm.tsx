1c20ac5237118ac264e0828a3b9b86c5bff5487c
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-10-09 16:46:04 -0400

    Change the textarea element to have rounded corners for a nicer appearance

--- app/components/instructionCreateForm.tsx
+++ app/components/instructionCreateForm.tsx
@@ -7,7 +7,7 @@ const InstructionCreateForm = () => {
   return (
     <form action={getAnswer} className='flex flex-col items-center w-full lg:w-1/3'>
       <label htmlFor='instruction'>Instruction</label>
-      <textarea id='instruction' name='instruction' className='bg-white/20 mt-1 mb-5 resize-none w-full h-20 px-2 py-1' />
+      <textarea id='instruction' name='instruction' className='bg-white/20 mt-1 mb-5 resize-none w-full h-20 px-2 py-1 rounded' />
       <SubmitButton label='Send' loading='Searching...' />
     </form>
   );
