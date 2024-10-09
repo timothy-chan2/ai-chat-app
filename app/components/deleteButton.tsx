db788a6e231bf61f44c900b5b6ab1e9e63cc3576
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-10-09 16:43:46 -0400

    Add styles to the delete button

--- app/components/deleteButton.tsx
+++ app/components/deleteButton.tsx
@@ -4,7 +4,7 @@ const DeleteButton = ({ label, loading }: { label: string, loading: string }) =>
   const { pending } = useFormStatus();
 
   return (
-    <button disabled={pending} type='submit'>
+    <button disabled={pending} type='submit' className='bg-red-500 hover:bg-red-700 font-semibold py-1 px-5 rounded mt-2 mb-2'>
       {pending ? loading : label}
     </button>
   );
