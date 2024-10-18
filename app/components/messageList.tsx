aa1e554bf3bf6cbbf1e681a31a9ab4b2ba12cd8d
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-10-17 23:53:59 -0400

    Update so the instructions entered by the user is aligned to the right side of the screen to more easily distinguish between AI and user content

--- app/components/messageList.tsx
+++ app/components/messageList.tsx
@@ -8,13 +8,28 @@ const MessageList = async () => {
   return (
     numberOfMessages > 0 &&
     <ul className='w-full lg:w-1/3 mb-8'>
-      {messages.map(message => (
-        <li key={message.id} className='mb-3'>
-          <p className='font-bold'>{ message.creator }</p>
-          <p>{ message.text }</p>
-          <DeleteButtonContainer id={message.id} />
-        </li>
-      ))}
+      {messages.map(message => {
+        let style;
+        
+        switch (message.creator) {
+          case 'AI':
+            style = 'mb-3';
+            break;
+          case 'User':
+            style = 'mb-3 flex justify-end';
+            break;
+        }
+        
+        return (
+          <li key={message.id} className={style}>
+            <div>
+              <p className='font-bold'>{ message.creator }</p>
+              <p>{ message.text }</p>
+              <DeleteButtonContainer id={message.id} />
+            </div>
+          </li>
+        )
+      })}
     </ul>
   );
 };
