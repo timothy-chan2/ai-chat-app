fb6c3e3c97d57a54362400e52d69757bf74d63be
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-10-17 23:59:55 -0400

    Move the map function out of the return since it is getting long

--- app/components/messageList.tsx
+++ app/components/messageList.tsx
@@ -4,32 +4,33 @@ import { DeleteButtonContainer } from './deleteButtonContainer';
 const MessageList = async () => {
   const messages = await getMessages();
   const numberOfMessages = messages.length;
+  const messageList = messages.map(message => {
+    let style;
+    
+    switch (message.creator) {
+      case 'AI':
+        style = 'mb-3';
+        break;
+      case 'User':
+        style = 'mb-3 flex justify-end';
+        break;
+    }
+    
+    return (
+      <li key={message.id} className={style}>
+        <div>
+          <p className='font-bold'>{ message.creator }</p>
+          <p>{ message.text }</p>
+          <DeleteButtonContainer id={message.id} />
+        </div>
+      </li>
+    )
+  });
 
   return (
     numberOfMessages > 0 &&
     <ul className='w-full lg:w-1/3 mb-8'>
-      {messages.map(message => {
-        let style;
-        
-        switch (message.creator) {
-          case 'AI':
-            style = 'mb-3';
-            break;
-          case 'User':
-            style = 'mb-3 flex justify-end';
-            break;
-        }
-        
-        return (
-          <li key={message.id} className={style}>
-            <div>
-              <p className='font-bold'>{ message.creator }</p>
-              <p>{ message.text }</p>
-              <DeleteButtonContainer id={message.id} />
-            </div>
-          </li>
-        )
-      })}
+      {messageList}
     </ul>
   );
 };
