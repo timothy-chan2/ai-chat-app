599a42c8873d081b9a3079a20ff42aed9460bf4f
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-10-24 00:06:03 -0400

    Update so the textarea field is cleared after the user submits their instruction

--- app/components/instructionCreateForm.tsx
+++ app/components/instructionCreateForm.tsx
@@ -1,11 +1,27 @@
 'use client';
 
+import { useRef } from 'react';
+
 import { getAnswer } from '../actions';
 import { SubmitButton } from './submitButton';
 
 const InstructionCreateForm = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleCreateInstruction = async (formData: FormData) => {
+    await getAnswer(formData);
+
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+  };
+  
   return (
-    <form action={getAnswer} className='flex flex-col items-center w-full md:w-3/4 lg:w-1/3'>
+    <form
+      ref={formRef}
+      action={handleCreateInstruction}
+      className='flex flex-col items-center w-full md:w-3/4 lg:w-1/3'
+    >
       <label htmlFor='instruction'>Instruction</label>
       <textarea id='instruction' name='instruction' className='bg-white/20 mt-1 mb-5 resize-none w-full h-20 px-2 py-1 rounded' />
       <SubmitButton label='Send' loading='Searching...' />
